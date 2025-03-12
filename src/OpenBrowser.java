fd398db58790189f2968aad2ffa86f252b98970b
sachinhake <hakkesachin9@gmail.com>
2025-03-12 23:15:00 +0530

    new_Chromer_Driver_code

--- src/OpenBrowser.java
+++ src/OpenBrowser.java
@@ -1,32 +1,22 @@
-package com.example;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class OpenBrowser {
-
     public static void main(String[] args) {
-        // Set up WebDriverManager to manage the ChromeDriver version
-        WebDriverManager.chromedriver().setup();
+        // Set up the FirefoxDriver automatically using WebDriverManager
+        WebDriverManager.firefoxdriver().setup();
 
-        // Specify path to your downloaded ChromeDriver version manually if necessary
-        System.setProperty("webdriver.chrome.driver", "/Downloads/chrome-linux64 (1)/chrome-linux64"); // Update with your path
+        // Instantiate FirefoxDriver
+        WebDriver driver = new FirefoxDriver();
 
-        // Instantiate the ChromeDriver
-        WebDriver driver = new ChromeDriver();
-
-        // Open Google
+        // Open a website
         driver.get("https://www.google.com");
 
-        // Wait for a few seconds
-        try {
-            Thread.sleep(5000);  // Wait for 5 seconds
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // Perform your test steps here
+        System.out.println("Title of the page: " + driver.getTitle());
 
-        // Close the browser
-        //driver.quit();
+        // Quit the driver after the test
+        driver.quit();
     }
 }
